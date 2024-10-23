e9c990960513c45eae9dc83eb06e73afdc9f7d6f
Muhammad Jareer <jareerobaray@icloud.com>
2024-10-23 07:52:52 +0200

    Added new functions and changed function names
    I changed the functions name so that it is more readable and added a few functions to help calculate

--- index.js
+++ index.js
@@ -5,14 +5,23 @@
  * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
  */
 
-// Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+// Conversion constants
+const SECONDS_IN_HOUR = 3600;
+const KM_TO_M = 1000;
 
+// Given Parameters
+const initialVel = 10000; // velocity (km/h)
+const acc = 3; // acceleration (m/s^2)
+const timeInSeconds = 3600; // seconds (1 hour)
+const initialDistance = 0; // distance (km)
+const fuel = 5000; // remaining fuel (kg)
+const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
+
+// Helper: Convert verlocity from km/h to m/s
+const convertKmHtoMs = (velInKmH) => (velInKmH * KM_TO_M) / SECONDS_IN_HOUR;
+
+// Helper: Convert verlocity from m/s to km/h
+const convertMStoKms = (velInMS) => (velInMS * SECONDS_IN_HOUR) / KM_TO_M;
 
 const d2 = d + (vel*time) //calcultes new distance
 const rf = fbr*time //calculates remaining fuel
